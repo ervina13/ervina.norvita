0b418de01d7ebb4bc29e7b422ba809d75c30e287
ervina13 <86247885+ervina13@users.noreply.github.com>
2024-10-29 09:20:26 +0700

    CashierApp

---
+++ cashier-app/repository/transaction.go
@@ -0,0 +1,27 @@
+package repository
+
+type TransactionRepository struct {
+	cartItemRepository CartItemRepository
+	salesRepository    SalesRepository
+}
+
+func NewTransactionRepository(cartItemRepository CartItemRepository, salesRepository SalesRepository) TransactionRepository {
+	return TransactionRepository{cartItemRepository, salesRepository}
+}
+
+func (u *TransactionRepository) Pay(cartItems []CartItem, amount int) (int, error) {
+	//beginanswer
+	totalPrice, err := u.cartItemRepository.TotalPrice()
+	if err != nil {
+		return 0, err
+	}
+
+	moneyChanges := amount - totalPrice
+	err = u.salesRepository.Add(cartItems)
+	if err != nil {
+		return 0, err
+	}
+
+	return moneyChanges, nil
+	//endanswer return 0, nil
+}
